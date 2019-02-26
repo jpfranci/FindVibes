5501adac8a9116c81f969167654b333df57285ab
Jip <jpbfrancisco1998@gmail.com>
2019-02-26 01:47:16 -0800

    added node code

---
+++ spotify-recommend/functions/index.js
@@ -0,0 +1,50 @@
+/**
+ * This is an example of a basic node.js script that performs
+ * the Authorization Code oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
+ */
+
+var express = require('express'); // Express web server framework
+var functions = require('firebase-functions');
+var cors = require('cors');
+var querystring = require('querystring');
+
+var redirect_uri = 'https://find-vibes.firebaseapp.com/'; // Your redirect uri
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var app = express();
+app.use(cors());
+
+app.get('/login', function(req, res) {
+  var state = generateRandomString(16);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-top-read user-read-playback-state playlist-modify-private';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'token',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+exports.widgets = functions.https.onRequest(app); 
